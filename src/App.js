c5df81f6db639dfc80935d96a80a4192e3b78187
Pitt Wu <kgb00128@gmail.com>
2022-01-03 11:40:10 +0800

    feat: fetch data

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
-import "./App.css";
+import { useState, useEffect } from 'react';
+import Card from './components/Card';
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    const url = `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`;
+    const fetchImages = async () => {
+      await fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          setImages(data.hits);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    fetchImages();
+  }, [term]);
+
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl-mb-2">
-          Photo by John Name
-        </div>
-        <ul>
-          <li>
-            <strong>Views:</strong>
-            100000
-          </li>
-          <li>
-            <strong>Downloads:</strong>
-            300
-          </li>
-          <li>
-            <strong>Likes:</strong>
-            9000
-          </li>
-        </ul>
-      </div>
-      <div className="px-6 py-4">
-        <span className="inlin-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semiblod text-gray-700 mr-2">
-          # tag1
-        </span>
-        <span className="inlin-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semiblod text-gray-700 mr-2">
-          # tag2
-        </span>
-        <span className="inlin-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semiblod text-gray-700 mr-2">
-          # tag3
-        </span>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        {images.map((image) => {
+          return <Card key={image.id} data={image} />;
+        })}
       </div>
     </div>
   );
---
+++ src/components/Card.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+function Card({ data }) {
+  const { largeImageURL, user, likes, downloads, views, tags } = data;
+  const tagsData = tags.split(',');
+
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src={largeImageURL} alt="" className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl-mb-2">
+          Photo by {user}
+        </div>
+        <ul>
+          <li>
+            <strong>Views : </strong>
+            {views}
+          </li>
+          <li>
+            <strong>Downloads : </strong>
+            {downloads}
+          </li>
+          <li>
+            <strong>Likes : </strong>
+            {likes}
+          </li>
+        </ul>
+      </div>
+      <div className="px-6 py-4">
+        {tagsData.map((tag, index) => {
+          return (
+            <span
+              key={index}
+              className="inlin-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semiblod text-gray-700 mr-2"
+            >
+              {tag}
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Card;
